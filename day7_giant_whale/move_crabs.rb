12b88fb83634f4afba4e81a483068cea426e18d9
Enrico Gallus <enrico.gallus@gmail.com>
2021-12-11 17:54:52 +0900

    finished day 7 advanced

--- day7_giant_whale/move_crabs.rb
+++ day7_giant_whale/move_crabs.rb
@@ -1,21 +1,30 @@
-def move_crabs(file_name)
+def move_crabs(file_name, advanced)
   positions = File.read(file_name)
                   .split(',')
                   .map(&:to_i)
   min = positions.min
   max = positions.max
   mean = positions.sum / positions.length
-  lower = calculated_cheapest(min, mean, positions)
-  upper = calculated_cheapest(mean, max, positions)
-  [lower, upper].min
+  [calculated_cheapest(min, mean, positions, advanced), calculated_cheapest(mean, max, positions, advanced)].min
 end
 
-def calculated_cheapest(lower, upper, positions)
+def calculated_cheapest(lower, upper, positions, advanced)
   previous = nil
   (lower..upper).each do |level|
-    result = positions.reduce(0) { |sum, x| sum + (level - x).abs }
+    result = positions.reduce(0) { |sum, x| sum + get_step(level, x, advanced) }
     return previous if !previous.nil? && previous < result
 
     previous = result
+  end
+
+  previous
+end
+
+def get_step(level, num, advanced)
+  step = (level - num).abs
+  if advanced
+    step.zero? ? step : (1..step).reduce(0) { |sum, x| sum + x }
+  else
+    step
   end
 end
--- test/move_crabs_test.rb
+++ test/move_crabs_test.rb
@@ -5,10 +5,18 @@ require_relative '../day7_giant_whale/move_crabs'
 class MoveCrabsTest < Minitest::Test
 
   def test_move_crabs
-    assert_equal 37, move_crabs('day7_giant_whale/test_input.txt')
+    assert_equal 37, move_crabs('day7_giant_whale/test_input.txt', false)
   end
 
   def test_real_move_crabs
-    assert_equal 336131, move_crabs('day7_giant_whale/input.txt')
+    assert_equal 336131, move_crabs('day7_giant_whale/input.txt', false)
+  end
+
+  def test_move_crabs_advanced
+    assert_equal 168, move_crabs('day7_giant_whale/test_input.txt', true)
+  end
+
+  def test_real_move_crabs_advanced
+    assert_equal 92676646, move_crabs('day7_giant_whale/input.txt', true)
   end
 end
