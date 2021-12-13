dea9ce54b0c39e2256e6c973d5c1ce9696af9c14
Enrico Gallus <enrico.gallus@gmail.com>
2021-12-14 08:19:04 +0900

    finish advanced day 13

--- day13_origami/origami.rb
+++ day13_origami/origami.rb
@@ -1,4 +1,4 @@
-def origami(file_name)
+def origami(file_name, limit = nil)
   lines = File.readlines(file_name).map(&:strip).reject(&:empty?)
   instructions = lines.select { |line| line.start_with?('fold along') }
   dots = lines.reject { |line| line.start_with?('fold along') }.map { |e| e.split(',').map(&:to_i) }
@@ -6,23 +6,25 @@ def origami(file_name)
   max_y = dots.max_by { |a| a[1] }[1] + 1
   data = Array.new(max_y) { Array.new(max_x) }
   dots.each { |coord| data[coord[1]][coord[0]] = 1 }
+  limit = instructions.length if limit.nil?
 
-  instruction = instructions[0].gsub('fold along ', '').split('=')
-  if instruction[0] == 'y'
-    row = instruction[1].to_i
-    keep = data.slice(0, row)
-    data.slice(row + 1, max_y)
-               .reverse
-               .each_with_index { |y, y_index| y.each_with_index { |x, x_index| keep[y_index][x_index] = 1 unless x.nil? } }
-    data = keep
-  else
-    column = instruction[1].to_i
-    keep = data.transpose.slice(0, column)
-    data.transpose.slice(column + 1, max_x)
-        .reverse
-        .each_with_index { |y, y_index| y.each_with_index { |x, x_index| keep[y_index][x_index] = 1 unless x.nil? } }
-    data = keep.transpose
+  instructions.take(limit).each do |line|
+    instruction = line.gsub('fold along ', '').split('=')
+    fold_at = instruction[1].to_i
+    data = if instruction[0] == 'y'
+             fold_paper(data, fold_at, max_y)
+           else
+             fold_paper(data.transpose, fold_at, max_x).transpose
+           end
   end
 
   data.flatten.compact.sum
+end
+
+def fold_paper(data, fold_at, max)
+  keep = data.slice(0, fold_at)
+  data.slice(fold_at + 1, max)
+      .reverse
+      .each_with_index { |y, y_index| y.each_with_index { |x, x_index| keep[y_index][x_index] = 1 unless x.nil? } }
+  keep
 end
--- test/origami_test.rb
+++ test/origami_test.rb
@@ -1,12 +1,21 @@
 require 'minitest/autorun'
 require_relative '../day13_origami/origami'
+require_relative '../day13_origami/test'
 
 class OrigamiTest < Minitest::Test
+  def test_origami_mapping_one_step
+    assert_equal 17, origami('day13_origami/test_input.txt', 1)
+  end
+
+  def test_real_origami_mapping_one_step
+    assert_equal 781, origami('day13_origami/input.txt', 1)
+  end
+
   def test_origami_mapping
-    assert_equal 17, origami('day13_origami/test_input.txt')
+    assert_equal 16, origami('day13_origami/test_input.txt')
   end
 
   def test_real_origami_mapping
-    assert_equal 781, origami('day13_origami/input.txt')
+    assert_equal 138, origami('day13_origami/input.txt')
   end
 end
