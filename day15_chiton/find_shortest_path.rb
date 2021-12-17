b08c0bbed36a7b5ce2ebae33d176c8daf31dc9d8
Enrico Gallus <enrico.gallus@gmail.com>
2021-12-17 10:02:24 +0900

    using hash is still too slow

--- day15_chiton/find_shortest_path.rb
+++ day15_chiton/find_shortest_path.rb
@@ -1,17 +1,21 @@
 def find_path(file_name)
-  @board = File.readlines(file_name).map(&:strip).collect { |line| line.split('').map(&:to_i) }
+  board = File.readlines(file_name)
+              .map(&:strip)
+              .collect { |line| line.split('').map(&:to_i) }
+  @hashed_board = board.each.with_index.with_object({}) { |(x_array, y_index), h| x_array.each_with_index { |value, x_index| h["#{x_index},#{y_index}"] = value } }
   @x = board[0].length - 1
   @y = board.length - 1
   @lowest_cost = nil
-  possible_paths([0, 0], [@x, @y], 0)
+  possible_paths('0,0', "#{@x},#{@y}", 0)
   @lowest_cost
 end
 
 def possible_paths(current_coordinates, destination, cost_so_far)
-  current_value = @board[current_coordinates[0]][current_coordinates[1]]
+  current_value = @hashed_board[current_coordinates]
   if current_coordinates == destination
-    cost = cost_so_far + current_value - @board[0][0]
+    cost = cost_so_far + current_value - @hashed_board['0,0']
     @lowest_cost = cost if @lowest_cost.nil? || cost < @lowest_cost
+    puts @lowest_cost
   else
     adjacent_coords(current_coordinates).each do |adjacent_coord|
       possible_paths(adjacent_coord, destination, cost_so_far + current_value)
@@ -20,8 +24,9 @@ def possible_paths(current_coordinates, destination, cost_so_far)
 end
 
 def adjacent_coords(current_coord)
+  x, y = current_coord.split(',').map(&:to_i)
   adjacents = []
-  adjacents.push([current_coord[0] + 1, current_coord[1]]) if current_coord[0] < @y
-  adjacents.push([current_coord[0], current_coord[1] + 1]) if current_coord[1] < @x
+  adjacents.push("#{x + 1},#{y}") if x < @x
+  adjacents.push("#{x},#{y + 1}") if y < @y
   return adjacents
 end
