95779286115c879ae38b9ac807aa86eb37ab4f66
Enrico Gallus <enrico.gallus@gmail.com>
2021-12-18 10:46:35 +0900

    changed to use array instead of hash, but produce the same result

--- day15_chiton/find_cheapest_path.rb
+++ day15_chiton/find_cheapest_path.rb
@@ -1,36 +1,21 @@
 def find_cheapest_path(file_name)
   board = construct_board(file_name)
-  @x = board[0].length - 1
-  @y = board.length - 1
-  @hashed_board = board.each.with_index.with_object({}) { |(x_array, y_index), h| x_array.each_with_index { |value, x_index| h["#{x_index},#{y_index}"] = value } }
-  @hashed_board['0,0'] = 0
-  calculate_costs('0,0', "#{@x},#{@y}")
-  @hashed_board["#{@x},#{@y}"]
+  board[0][0] = 0
+  calculate_costs(board, [0, 0])
 end
 
 def five_time_larger(file_name)
   board = construct_board(file_name)
-  @x = board[0].length
-  @y = board.length
-  @hashed_board = board.each.with_index.with_object({}) { |(x_array, y_index), h| x_array.each_with_index { |value, x_index| h["#{x_index},#{y_index}"] = value } }
-  # fill x
-  (1..4).each do |x|
-    board.each_with_index.with_object(@hashed_board) { |(x_array, y_index), h| x_array.each_with_index { |value, x_index| h["#{x_index + (x * @x)},#{y_index}"] = value + x > 9 ? value + x - 9 : value + x } }
-  end
-  hash_board_clone = @hashed_board.clone
-  (1..4).each do |factor|
-    hash_board_clone.each do |key, value|
-      x, y = x_y(key)
-      @hashed_board["#{x},#{y + factor * @y}"] = value + factor > 9 ? value + factor - 9 : value + factor
-    end
 
+  # extend tiles
+  board.map { |line| line.concat((1..4).map { |x| line.map { |value| value + x > 9 ? value + x - 9 : value + x } }.flatten) }
+  new_board = board.clone
+  (1..4).each do |y|
+    board.select { |line| new_board.push(line.map { |value| value + y > 9 ? value + y - 9 : value + y }) }
   end
 
-  @x = @x * 5 - 1
-  @y = @y * 5 - 1
-  @hashed_board['0,0'] = 0
-  calculate_costs('0,0', "#{@x},#{@y}")
-  @hashed_board["#{@x},#{@y}"]
+  new_board[0][0] = 0
+  calculate_costs(new_board, [0, 0])
 end
 
 def construct_board(file_name)
@@ -39,29 +24,26 @@ def construct_board(file_name)
       .collect { |line| line.split('').map(&:to_i) }
 end
 
-def calculate_costs(current_coordinates, destination)
-  x, y = x_y(current_coordinates)
-  @hashed_board[current_coordinates] = cheapest_cost(x, y)
-  return if current_coordinates == destination
+def calculate_costs(board, current)
+  x, y = current
+  board[x][y] = cheapest_cost(board, x, y)
+  return board[x][y] if x == board[0].length - 1 && y == board.length - 1
 
-  (x + 1..@x).each do |x|
-    @hashed_board["#{x},#{y}"] = cheapest_cost(x, y)
+  (x + 1..(board[0].length - 1)).each do |x|
+    board[x][y] = cheapest_cost(board, x, y)
   end
 
-  (y + 1..@y).each do |y|
-    @hashed_board["#{x},#{y}"] = cheapest_cost(x, y)
+  (y + 1..(board.length - 1)).each do |y|
+    board[x][y] = cheapest_cost(board, x, y)
   end
 
-  calculate_costs("#{x + 1},#{y + 1}", destination)
+  calculate_costs(board, [x + 1, y + 1])
 end
 
-def cheapest_cost(x, y)
-  left = @hashed_board["#{x - 1},#{y}"]
-  upper = @hashed_board["#{x},#{y - 1}"]
+def cheapest_cost(board, x, y)
+  left = board[x - 1][y] if x.positive?
+  upper = board[x][y - 1] if y.positive?
   cheapest_adjacent = [left, upper].compact.min
-  cheapest_adjacent.nil? ? @hashed_board["#{x},#{y}"] : @hashed_board["#{x},#{y}"] + cheapest_adjacent
-end
-
-def x_y(string_coord)
-  string_coord.split(',').map(&:to_i)
+  current_value = board[x][y]
+  cheapest_adjacent.nil? ? current_value : current_value + cheapest_adjacent
 end
