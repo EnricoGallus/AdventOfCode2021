fe4612730bd65f794be80d950bd88b157b7318b1
Enrico Gallus <enrico.gallus@gmail.com>
2021-12-18 12:07:57 +0900

    fail to accomplish optimization in polymere

--- test/polymer_test.rb
+++ test/polymer_test.rb
@@ -7,15 +7,23 @@ class PolymerTest < Minitest::Test
     assert_equal 1, pair_insertion('day14_polymer/test_input.txt', 1)
   end
 
+  def test_pair_insertion_twice
+    assert_equal 5, pair_insertion('day14_polymer/test_input.txt', 2)
+  end
+
   def test_pair_insertion_ten_times
     assert_equal 1588, pair_insertion('day14_polymer/test_input.txt', 10)
+  end
+
+  def test_pair_insertion_fourty_times
+    assert_equal 2188189693529, pair_insertion('day14_polymer/test_input.txt', 40)
   end
 
   def test_real_pair_insertion_ten_times
     assert_equal 2947, pair_insertion('day14_polymer/input.txt', 10)
   end
 
-  def test_real_pair_insertion_ten_times
+  def test_real_pair_insertion_fourty_times
     assert_equal 2947, pair_insertion('day14_polymer/input.txt', 40)
   end
 end
