c5a735531dc6c7be5ecb89904731e1d95b6bfd80
Enrico Gallus <enrico.gallus@gmail.com>
2021-12-19 09:15:52 +0900

    algorithm does not work for on case (example 2)

--- day16_package_decoder/package_decoder.rb
+++ day16_package_decoder/package_decoder.rb
@@ -1,3 +1,52 @@
 def package_decoder(file_name)
+  decoded = decode(File.read(file_name))
+  packages = []
+  parse(decoded, packages) until decoded.empty?
+  packages
+end
 
+def decode(content)
+  content.split('').map { |char| char.hex.to_s(2).rjust(4, '0') }.join.split('')
+end
+
+def parse(content, packages, continue_parse = true)
+  return if content.empty?
+
+  version = content.shift(3).join.to_i(2)
+  type = content.shift(3).join.to_i(2)
+  length_type = nil
+  case type
+  when 4 # literal
+    group_length = content.length
+    groups = read_literal_group(content)
+    packages.push([version, type, length_type, groups.flatten.join.to_i(2)])
+    content.shift(group_length % 5) if content.length <= 6
+    parse(content, packages, continue_parse) if continue_parse
+  else # operator
+    length_type = content.shift(1)[0].to_i
+    packages.push([version, type, length_type, nil])
+    case length_type
+    when 0
+      subpackages = content.shift(content.shift(15).join.to_i(2))
+      parse(subpackages, packages, continue_parse)
+    when 1
+      subpackages_count = content.shift(11).join.to_i(2)
+      subpackages_count.times do
+        parse(content, packages, false)
+      end
+      continue_parse = true
+    end
+  end
+end
+
+def read_literal_group(content)
+  groups = []
+  continue_read = true
+  while continue_read
+    group = content.shift(5)
+    next_group_bit = group[0]
+    groups.push(group[1..4])
+    continue_read = next_group_bit == 1
+  end
+  groups
 end
--- test/package_decoder_test.rb
+++ test/package_decoder_test.rb
@@ -1,8 +1,87 @@
 require 'minitest/autorun'
+require_relative '../day16_package_decoder/package_decoder'
 
 class PackageDecoderTest < Minitest::Test
 
   def test_decoding_package
+    assert_equal %w[1 1 0 1 0 0 1 0 1 1 1 1 1 1 1 0 0 0 1 0 1 0 0 0 0 0 0 0], decode('D2FE280')
+  end
 
+  def test_parse_literal
+    packages = []
+    parse('110100101111111000101000'.split('').map(&:to_i), packages)
+    assert_equal 1, packages.length
+    assert_equal [6, 4, nil, 2021], packages[0]
+  end
+
+  def test_parse_operator_type_0
+    packages = []
+    parse('00111000000000000110111101000101001010010001001000000000'.split('').map(&:to_i), packages)
+    assert_equal 3, packages.length
+    assert_equal [1, 6, 0, nil], packages[0]
+    assert_equal [6, 4, nil, 10], packages[1]
+    assert_equal [2, 4, nil, 20], packages[2]
+  end
+
+  def test_parse_operator_type_1
+    packages = []
+    parse('11101110000000001101010000001100100000100011000001100000'.split('').map(&:to_i), packages)
+    assert_equal 4, packages.length
+    assert_equal [7, 3, 1, nil], packages[0]
+    assert_equal [2, 4, nil, 1], packages[1]
+    assert_equal [4, 4, nil, 2], packages[2]
+    assert_equal [1, 4, nil, 3], packages[3]
+  end
+
+  def test_decode_example_1
+    binary = decode('8A004A801A8002F478')
+    packages = []
+    parse(binary, packages)
+
+    assert_equal 4, packages.length
+    assert_equal [4, 2, 1, nil], packages[0]
+    assert_equal [1, 2, 1, nil], packages[1]
+    assert_equal [5, 2, 0, nil], packages[2]
+    assert_equal [6, 4, nil, 15], packages[3]
+  end
+
+  def test_decode_example_2
+    binary = decode('620080001611562C8802118E34')
+    packages = []
+    parse(binary, packages)
+
+    assert_equal 6, packages.length
+    assert_equal [3, 0, 1, nil], packages[0]
+    assert_equal [1, 2, 1, nil], packages[1]
+    assert_equal [5, 2, 0, nil], packages[2]
+    assert_equal [6, 4, nil, 15], packages[3]
+  end
+
+  def test_decode_example_3
+    binary = decode('C0015000016115A2E0802F182340')
+    packages = []
+    parse(binary, packages)
+
+    assert_equal 6, packages.length
+    assert_equal [4, 2, 1, nil], packages[0]
+    assert_equal [1, 2, 1, nil], packages[1]
+    assert_equal [5, 2, 0, nil], packages[2]
+    assert_equal [6, 4, nil, 15], packages[3]
+  end
+
+  def test_decode_example_3
+    binary = decode('A0016C880162017C3686B18A3D4780')
+    packages = []
+    parse(binary, packages)
+
+    assert_equal 8, packages.length
+    assert_equal [5, 0, 0, nil], packages[0]
+    assert_equal [1, 0, 1, nil], packages[1]
+    assert_equal [3, 0, 1, nil], packages[2]
+    assert_equal [7, 4, nil, 6], packages[3]
+    assert_equal [6, 4, nil, 6], packages[4]
+    assert_equal [5, 4, nil, 12], packages[5]
+    assert_equal [2, 4, nil, 15], packages[6]
+    assert_equal [2, 4, nil, 15], packages[7]
   end
 end
